57213b439dd142981e4a775e8995561cd226843c
akilinjamam <akilinjamam@gmail.comm>
2022-04-07 00:36:58 +0600

    adding-files

---
+++ src/_steps.js
@@ -0,0 +1,14 @@
+/* 
+steps to use firebase
+
+1.create a project on console.firebase.google.com.
+2.npm install firebase 
+3.Register web app in firebase
+4.copy firebase init with config from firebase project settings into a file firebase.init.js
+5.import app firebase init.js into your app.js
+6.export default app from firebase.init.js
+7.import getAuth from firebase/ auth and create auth = getAuth(app)
+8.turn on google authentication(firebase > authentication > google sign in)
+9.create google provider
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,62 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCwZX0Y7YUlhpjFD3G1ZyEpOLteOOB1HKg",
+    authDomain: "simple-firebase-authenti-5a216.firebaseapp.com",
+    projectId: "simple-firebase-authenti-5a216",
+    storageBucket: "simple-firebase-authenti-5a216.appspot.com",
+    messagingSenderId: "339693063202",
+    appId: "1:339693063202:web:d1297237ca83ace0b30e7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCwZX0Y7YUlhpjFD3G1ZyEpOLteOOB1HKg",
+    authDomain: "simple-firebase-authenti-5a216.firebaseapp.com",
+    projectId: "simple-firebase-authenti-5a216",
+    storageBucket: "simple-firebase-authenti-5a216.appspot.com",
+    messagingSenderId: "339693063202",
+    appId: "1:339693063202:web:d1297237ca83ace0b30e7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app */
